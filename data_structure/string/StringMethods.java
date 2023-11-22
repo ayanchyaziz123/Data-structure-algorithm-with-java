e77648a5d7f33deb68681d482fd17099c5f136d3
azizchyayan123 <aaziz9642@gmail.com>
2023-11-22 19:17:40 +0600

    added

--- data_structure/string/StringMethods.java
+++ data_structure/string/StringMethods.java
@@ -1,13 +1,17 @@
 package String;
 
 public class StringMethods {
-
-    public static void printf(Object object)
-    {
+    // String most used built n function
+    // Lets definre a variable st = "hello world..!"
+    // 1. st.length()
+    // 2. st.charAt(index) return the value of index
+    // 3. st.toCharArray()
+    // char[] arr = st.toCharArray();
+    // for(char c: st.toCharArray())
+    public static void printf(Object object){
         System.out.print(object);
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         String st = "hellow world";
         for(int i = 0; i < st.length(); i++)
         {
@@ -15,6 +19,5 @@ public class StringMethods {
             printf(c + "\n");
         }
         printf(st.codePointAt(0));
-        
     }
 }
---
+++ oop/GenericClass.java
@@ -0,0 +1,15 @@
+public class GenericClass<T> {
+    class Node{
+        public Node(T data)
+        {
+            
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        GenericClass<Integer> gc = new GenericClass<Integer>();
+
+    }
+    
+}
